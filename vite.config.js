071bf0e4100bdb97f6461afa8c5faaf51fb090d9
suminb99 <suminbaek99@gmail.com>
2025-12-23 15:38:22 +0900

    chore: 프로젝트 환경 및 설정 파일 추가

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import {defineConfig} from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+import path from 'path';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@components': path.resolve(__dirname, './src/components'),
+      '@apis': path.resolve(__dirname, './src/apis'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@constants': path.resolve(__dirname, './src/constants'),
+      '@layout': path.resolve(__dirname, './src/layout'),
+      '@lib': path.resolve(__dirname, './src/lib'),
+      '@pages': path.resolve(__dirname, './src/pages'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+      '@mock': path.resolve(__dirname, './src/mock'),
+      '@data': path.resolve(__dirname, './src/data'),
+    },
+  },
+  server: {
+    host: '0.0.0.0', // 외부 접속 허용
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
+    },
+  },
+});
